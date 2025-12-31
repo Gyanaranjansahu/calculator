28d10a99887e52dbecc3ef6c5965151ee420f72b
Gyanaranjan sahu <gyana9619@gmail.com>
2025-12-31 21:06:28 +0530

    Add initial CSS styles for layout and buttons

---
+++ file.js
@@ -0,0 +1,50 @@
+body{
+    height: 100dvh;
+    width: 100dvw;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background:linear-gradient(30deg,rgb(4, 4, 4),blue,rgb(76, 2, 14))
+}
+.container{
+    border: 1px solid black;
+    padding: 50px;
+    border-radius: 30px;
+    box-shadow: 0px 10px 10px 10px;
+}
+#input{
+    width: 100%;
+    height: 100px;
+    border-radius: 30px;
+    text-align: right;
+    border: none;
+     background: transparent;
+     font-size: larger;
+        color: white;
+         font-size:30px;
+}
+
+input::placeholder{
+    color: white;
+    font-size: 50px;
+}
+
+button{
+    height: 60px;
+    width: 60px;
+    margin: 15px;
+    font-size: larger;
+    border-radius: 50%;
+    box-shadow: 0px 0px 10px 10px;
+    text-align: center;
+    font-weight: bold;
+
+
+}
+#equal{
+    background-color: rgb(255, 255, 0);
+}
+.s{
+   color : rgb(5, 246, 5);
+    background: black;
+}
